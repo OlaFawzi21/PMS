3f1aeaed5c49b19c4fdb7c22cfbbbbd07903953c
Ola Fawzi <olafawzii80@gmail.com>
2024-08-19 23:58:59 +0300

    update routing

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthComponent } from './auth.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { ForgetPassComponent } from './components/forget-pass/forget-pass.component';
