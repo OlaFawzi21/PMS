0513f4096b7a402891868bf2b16b48982832dc20
reemWael123 <reemelkazaz@gmail.com>
2024-08-30 17:32:27 +0300

    first

--- src/app/dashboard/Modules/manager/users/services/user.service.ts
+++ src/app/dashboard/Modules/manager/users/services/user.service.ts
@@ -1,13 +1,40 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private _HttpClient: HttpClient) {}
+  constructor(private _HttpClient: HttpClient,private _toastr:ToastrService) {}
   getUsers(parms: any): Observable<any> {
     return this._HttpClient.get(`Users`, { params: parms });
+  }
+  getuserById(id: number):Observable<any>{
+    return this._HttpClient.get(`Users/${id}`);
+  }
+  imageUrl:any='https://upskilling-egypt.com:3003/'
+  getImagePath(imagePath: string): string {
+    
+    return this.imageUrl + imagePath;
+  }
+  loadImage(imagePath: string, files: any[]): void {
+    // Construct the full URL to the image
+    const imageUrl = this.getImagePath(imagePath);
+
+    // Fetch the image
+    fetch(imageUrl)
+      .then((response) => response.blob())
+      .then((blob) => {
+        // Create a File object from the Blob
+        const file = new File([blob], 'image.jpg', { type: blob.type });
+
+        // Set the file in the dropzone
+        files.push(file);
+      })
+      .catch((error) => {
+        this._toastr.error('Failed to load image');
+      });
   }
 }
--- src/app/dashboard/Modules/manager/users/users.module.ts
+++ src/app/dashboard/Modules/manager/users/users.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ViewComponent } from './components/view/view.component';
 
 
 @NgModule({
   declarations: [
-    UsersComponent
+    UsersComponent,
+    ViewComponent
   ],
   imports: [
     CommonModule,
