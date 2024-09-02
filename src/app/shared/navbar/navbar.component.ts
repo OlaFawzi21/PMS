d8158c0d2bc26206e2ebf8cdd0a4065f7cc3c4bc
Kareem Nasser <151379758+KareemNasser08@users.noreply.github.com>
2024-09-02 19:11:55 +0300

    Show / update profile without showing profile image

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
-  Username=localStorage.getItem('userName')
-  useremail=localStorage.getItem('userEmail')
+export class NavbarComponent implements OnInit{
+  Username = localStorage.getItem('userName');
+  useremail = localStorage.getItem('userEmail');
+  
+  // userId:number=0;
+  
+  logout() {
+    localStorage.removeItem('userToken');
+    localStorage.removeItem('userEmail');
+    localStorage.removeItem('role');
+    localStorage.clear();
+    this._Router.navigate(['/auth/login']);
+  }
+
+
+  constructor(
+    private _Router: Router,
+    private _AuthService: AuthService
+  ) { 
+  }
+
+  ngOnInit(): void {
+    // this._AuthService.getProfile();
+    // this.userId = Number(localStorage.getItem('userId'));
+  }
+
+  
+
+
 }
